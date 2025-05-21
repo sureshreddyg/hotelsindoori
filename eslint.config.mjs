1eda719162415faf64f9107dc6b4b8c6f69d5146
ls-madhu <madhureddy.vinta@gmail.com>
2025-05-21 22:37:43 +0530

    feat: configure eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import perfectionist from 'eslint-plugin-perfectionist';
 
-export default withNuxt(
-  // Your custom configs here
-)
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(perfectionist.configs['recommended-natural']);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
   srcDir: 'src/',
-})
+});
