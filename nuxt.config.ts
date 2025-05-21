9d7a8c825c5c28d2c21a44917c97db7f42371933
ls-madhu <madhureddy.vinta@gmail.com>
2025-05-21 23:30:49 +0530

    feat: configure tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
   srcDir: 'src/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
